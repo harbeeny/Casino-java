11ee001cb8c2b1b93b636c499093c6351f2990d0
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2024-03-04 11:24:47 -0500

    refactored dupes in blackjack

--- app/src/main/java/casino/blackjack/BlackJack.java
+++ app/src/main/java/casino/blackjack/BlackJack.java
@@ -4,50 +4,41 @@ import casino.shared.*;
 import casino.util.ReadInputFromUser;
 import java.util.ArrayList;
 
-    // #TODO: How can you remove duplicates in this code?  
-
 public class BlackJack {
     Deck deck = new Deck();
-    
+
     // dealer
     Card2 hiddenCard;
     ArrayList<Card2> dealerHand;
     int dealerSum;
     int dealerAceCount;
-    
+
     // player
     ArrayList<Card2> playerHand;
-    
+
     public BlackJack() {
     }
-    
+
     public void startGame() {
         // shuffle
         deck.shuffleDeck();
-      
+
         // dealer
-        dealerHand = new ArrayList<Card2>();
-        for (int i = 0; i < 2; i++) {
-            Card2 card = deck.takeCard();
-            dealerHand.add(card);
-        }
+        dealerHand = new ArrayList<>();
+        dealCards(dealerHand, 2);
         System.out.println("DEALER: ");
         System.out.println("[" + dealerHand.get(0) + "]"); // hidden card
         System.out.println(dealerHand.get(1));
-        System.out.println(dealerSum());
-        
+        System.out.println(calculateHandSum(dealerHand));
+
         // player
-        playerHand = new ArrayList<Card2>();
-        
-        for (int i = 0; i < 2; i++) {
-            Card2 card = deck.takeCard();
-            playerHand.add(card);
-        }
-        
-        System.err.println("PLAYER: ");
+        playerHand = new ArrayList<>();
+        dealCards(playerHand, 2);
+
+        System.out.println("PLAYER: ");
         System.out.println(playerHand);
-        System.out.println(playerSum());
-        
+        System.out.println(calculateHandSum(playerHand));
+
         System.out.println("Hit or Stay? (h/s)");
         String input = ReadInputFromUser.read();
         if (input.equals("h")) {
@@ -57,18 +48,26 @@ public class BlackJack {
         }
     }
 
+    private void dealCards(ArrayList<Card2> hand, int numberOfCards) {
+        for (int i = 0; i < numberOfCards; i++) {
+            Card2 card = deck.takeCard();
+            hand.add(card);
+        }
+    }
+
     private void hit() {
-        if (playerSum() < 21) {
+        if (calculateHandSum(playerHand) < 21) {
             Card2 card = deck.takeCard();
             playerHand.add(card);
-            
-            int playerSum = playerSum();
+
+            int playerSum = calculateHandSum(playerHand);
             if (playerSum > 21) {
-                System.out.println("BUST! Player's total is " + playerSum);
+                System.out.println("Player Hit and recieves: " + card);
+                System.out.println("BUST! Player's total is: " + playerSum + " :(");
             } else {
                 System.out.println("Hit and recieves: " + card);
-                System.out.println("New total:" + playerSum);
-                
+                System.out.println("New total: " + playerSum);
+
                 System.out.println("Hit or Stay? (h/s)");
                 String input = ReadInputFromUser.read();
                 if (input.equals("h")) {
@@ -79,25 +78,25 @@ public class BlackJack {
             }
         }
     }
-    
+
     private void stay() {
-        System.out.println("STAY:");
+        System.out.println("STAY: ");
         dealerPlays();
         whoWins();
     }
-    
+
     private void dealerPlays() {
-        while (dealerSum() < 17) {
+        while (calculateHandSum(dealerHand) < 17) {
             Card2 card = deck.takeCard();
             dealerHand.add(card);
             System.out.println("Dealer draws:" + card);
         }
-        System.out.println("Dealer stays with a hand of " + dealerSum());
+        System.out.println("Dealer stays with a hand of " + calculateHandSum(dealerHand));
     }
 
-    private int playerSum() {
+    private int calculateHandSum(ArrayList<Card2> hand) {
         int sum = 0;
-        for (Card2 card : playerHand) {
+        for (Card2 card : hand) {
             if (card.isAce() && sum + 11 > 21) {
                 sum += 1;
             } else if (card.isAce()) {
@@ -111,25 +110,9 @@ public class BlackJack {
         return sum;
     }
 
-    private int dealerSum() {
-        int sum = 0;
-        for (Card2 card : dealerHand) {
-            if (card.isAce() && sum + 11 > 21) {
-                sum += 1;
-            } else if (card.isAce()) {
-                sum += 11;
-            } else if (card.isKing() || card.isQueen() || card.isJack()) {
-                sum += 10;
-            } else {
-                sum += card.getValue();
-            }
-        }
-        return sum;
-    }
-    
     private void whoWins() {
-        int playerSum = playerSum();
-        int dealerSum = dealerSum();
+        int playerSum = calculateHandSum(playerHand);
+        int dealerSum = calculateHandSum(dealerHand);
         if (playerSum > 21) {
             System.out.println("Player Busts. Dealer wins");
         } else if (dealerSum > 21 || playerSum > dealerSum) {
